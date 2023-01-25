abfbacf78902a82ed754bcde3ae2c762c8422850
Matthew Ballard <matt@mrb.email>
2023-01-25 14:21:27 -0500

    should fail

--- Hw01.lean
+++ Hw01.lean
@@ -4,13 +4,11 @@ Find explicit terms of the following types
 
 variable (α β γ : Type) 
 
--- def prob01 : (α → β) → (β → γ) → (α → γ) := α 
+def prob01 : (α → β) → (β → γ) → (α → γ) := α 
 
--- def prob02 : (α → β → γ) → (α → β) → (α → γ) := sorry  
+def prob02 : (α → β → γ) → (α → β) → (α → γ) := sorry  
 
--- def prob03 : (α → α → β) → (β → γ) → α → β → γ := sorry 
-
-#check Nat
+def prob03 : (α → α → β) → (β → γ) → α → β → γ := sorry 
 
 /- 
 Below is a definition of an unlabeled binary tree in Lean. 
